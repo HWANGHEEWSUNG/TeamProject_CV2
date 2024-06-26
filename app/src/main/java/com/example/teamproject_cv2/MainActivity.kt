1f94f532217b32896e6cd548106ba0ad4a9a1f1c
희성 <ghkdgmltjd33@naver.com>
2024-06-26 16:50:31 +0900

    Initial commit

---
+++ app/src/main/java/com/example/teamproject_cv2/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.teamproject_cv2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.teamproject_cv2.loginScreen.LoginScreen
+import com.example.teamproject_cv2.ui.theme.TeamProject_CV2Theme
+import com.google.firebase.FirebaseApp
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.ktx.Firebase
+
+class MainActivity : ComponentActivity() {
+    private lateinit var analytics: FirebaseAnalytics
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Firebase 초기화
+        FirebaseApp.initializeApp(this)
+
+        analytics = Firebase.analytics
+
+        setContent {
+            TeamProject_CV2Theme {
+                AppContent()
+            }
+        }
+    }
+}
+
+@Composable
+fun AppContent() {
+    val navController = rememberNavController()
+    NavHost(navController, startDestination = "loginScreen") {
+        composable("loginScreen") { LoginScreen(navController) }
+        // Add other composable destinations here
+    }
+}
