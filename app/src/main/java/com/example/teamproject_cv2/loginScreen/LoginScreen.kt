aec69ed4aa3ef4e67b65571b56f9198a0e9315b2
희성 <ghkdgmltjd33@naver.com>
2024-07-04 16:13:08 +0900

    디자인 수정

--- app/src/main/java/com/example/teamproject_cv2/loginScreen/LoginScreen.kt
+++ app/src/main/java/com/example/teamproject_cv2/loginScreen/LoginScreen.kt
@@ -2,36 +2,46 @@ package com.example.teamproject_cv2.loginScreen
 
 import android.widget.Toast
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.blur
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
@@ -41,10 +51,11 @@ import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.tasks.await
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun LoginScreen(navController: NavController) {
-    val email = "1whrhddydrPwjd@gmail.com"
-    val password = "qwer123$"
+    var email by remember { mutableStateOf("1whrhddydrPwjd@gmail.com") }
+    var password by remember { mutableStateOf("qwer123$") }
     val auth: FirebaseAuth = Firebase.auth
     val context = LocalContext.current
     val coroutineScope = rememberCoroutineScope()
@@ -52,13 +63,26 @@ fun LoginScreen(navController: NavController) {
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color.White, RoundedCornerShape(12.dp))
-            .border(1.dp, Color(0xFFE8ECF4), RoundedCornerShape(12.dp))
+            .background(Color(0xFF1B0A40))
     ) {
-        // BackButton 컴포저블을 사용하여 뒤로 가기 버튼을 만듭니다.
-        BackButton {
-            navController.popBackStack()
-        }
+        // Background Ellipses
+        Box(
+            modifier = Modifier
+                .size(286.dp, 528.dp)
+                .offset(x = (-62).dp, y = (-69).dp)
+                .background(Color(0xFFFF43A8).copy(alpha = 0.19f), shape = CircleShape)
+                .blur(130.dp)
+        )
+        Box(
+            modifier = Modifier
+                .size(286.dp, 528.dp)
+                .offset(x = 322.dp, y = (-352).dp)
+                .background(Color(0xFFD943FF).copy(alpha = 0.19f), shape = CircleShape)
+                .blur(130.dp)
+                .graphicsLayer {
+                    rotationZ = 89.96f
+                }
+        )
 
         Column(
             modifier = Modifier
@@ -67,77 +91,93 @@ fun LoginScreen(navController: NavController) {
             verticalArrangement = Arrangement.SpaceBetween,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Spacer(modifier = Modifier.weight(1f))
-
-            // Inner column to center the main components
             Column(
                 horizontalAlignment = Alignment.CenterHorizontally,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .fillMaxHeight(0.5f),
-                verticalArrangement = Arrangement.Center
+                modifier = Modifier.fillMaxWidth()
             ) {
+                Spacer(modifier = Modifier.height(80.dp))
                 Text(
-                    text = "Welcome back! Glad to see you, Again!",
+                    text = "Welcome back",
                     fontFamily = FontFamily.SansSerif,
-                    fontSize = 30.sp,
-                    color = Color(0xFF1E232C),
-                    modifier = Modifier.padding(bottom = 16.dp)
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 28.sp,
+                    color = Color(0xFFEAE2FC)
                 )
-
-                OutlinedTextField(
-                    value = email,
-                    onValueChange = {},
-                    label = { Text("Email") },
-                    modifier = Modifier.fillMaxWidth(),
-                    enabled = false,
-                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
-                    colors = OutlinedTextFieldDefaults.colors(
-                        focusedBorderColor = Color(0xFFE8ECF4),
-                        unfocusedBorderColor = Color(0xFFE8ECF4),
-                        disabledBorderColor = Color(0xFFE8ECF4),
-                        disabledLabelColor = Color(0xFF8391A1),
-                        disabledTextColor = Color(0xFF8391A1)
-                    )
+                Spacer(modifier = Modifier.height(20.dp))
+                Text(
+                    text = "Lorem ipsum dolor sit amet, consectetur \n" +
+                            "adipisicing elit, sed do eiusmod.",
+                    fontFamily = FontFamily.Default,
+                    fontWeight = FontWeight.Medium,
+                    fontSize = 12.sp,
+                    color = Color(0xFFB8A8DB),
+                    textAlign = TextAlign.Center
                 )
-
-                Spacer(modifier = Modifier.height(8.dp))
-
-                OutlinedTextField(
-                    value = password,
-                    onValueChange = {},
-                    label = { Text("Enter your password") },
-                    modifier = Modifier.fillMaxWidth(),
-                    enabled = false,
-                    visualTransformation = PasswordVisualTransformation(),
-                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
-                    colors = OutlinedTextFieldDefaults.colors(
-                        focusedBorderColor = Color(0xFFE8ECF4),
-                        unfocusedBorderColor = Color(0xFFE8ECF4),
-                        disabledBorderColor = Color(0xFFE8ECF4),
-                        disabledLabelColor = Color(0xFF8391A1),
-                        disabledTextColor = Color(0xFF8391A1)
-                    )
-                )
-                Spacer(modifier = Modifier.height(4.dp))
-                Row(
+                Spacer(modifier = Modifier.height(40.dp))
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
                     modifier = Modifier
-                        .fillMaxWidth(),
-                    horizontalArrangement = Arrangement.End
+                        .fillMaxWidth()
+                        .height(171.dp)
                 ) {
-                    TextButton(onClick = { navController.navigate("forgotPasswordScreen") }) {
-                        Text(
-                            text = "Forgot Password?",
-                            fontFamily = FontFamily.Default,
-                            fontWeight = FontWeight.Medium,
-                            fontSize = 14.sp,
-                            color = Color(0xFF1E232C),
-                            modifier = Modifier
-                                .padding(bottom = 16.dp)
-                        )
+                    OutlinedTextField(
+                        value = email,
+                        onValueChange = { email = it },
+                        label = { Text("Email") },
+                        modifier = Modifier.fillMaxWidth(),
+                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+                        colors = TextFieldDefaults.outlinedTextFieldColors(
+                            focusedBorderColor = Color(0xFFEAE2FC),
+                            unfocusedBorderColor = Color(0xFFEAE2FC),
+                            cursorColor = Color(0xFFEAE2FC),
+                            focusedLabelColor = Color(0xFFB8A8DB),
+                            unfocusedLabelColor = Color(0xFFB8A8DB)
+                        ),
+                        shape = RoundedCornerShape(18.dp)
+                    )
+                    Spacer(modifier = Modifier.height(8.dp))
+                    OutlinedTextField(
+                        value = password,
+                        onValueChange = { password = it },
+                        label = { Text("Password") },
+                        modifier = Modifier.fillMaxWidth(),
+                        visualTransformation = PasswordVisualTransformation(),
+                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+                        colors = TextFieldDefaults.outlinedTextFieldColors(
+                            focusedBorderColor = Color(0xFFEAE2FC),
+                            unfocusedBorderColor = Color(0xFFEAE2FC),
+                            cursorColor = Color(0xFFEAE2FC),
+                            focusedLabelColor = Color(0xFFB8A8DB),
+                            unfocusedLabelColor = Color(0xFFB8A8DB)
+                        ),
+                        shape = RoundedCornerShape(18.dp)
+                    )
+                    Spacer(modifier = Modifier.height(4.dp))
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(end = 8.dp),
+                        horizontalArrangement = Arrangement.End
+                    ) {
+                        TextButton(onClick = { navController.navigate("forgotPasswordScreen") }) {
+                            Text(
+                                text = "Forgot Password?",
+                                fontFamily = FontFamily.SansSerif,
+                                fontWeight = FontWeight.Bold,
+                                fontSize = 14.sp,
+                                color = Color(0xFFEAE2FC)
+                            )
+                        }
                     }
                 }
+            }
 
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                contentAlignment = Alignment.BottomEnd
+            ) {
                 Button(
                     onClick = {
                         coroutineScope.launch {
@@ -154,20 +194,18 @@ fun LoginScreen(navController: NavController) {
                         }
                     },
                     modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(8.dp)
-                        .height(60.dp),
+                        .width(171.dp)
+                        .height(60.dp)
+                        .padding(bottom = 15.dp),
                     colors = ButtonDefaults.buttonColors(
-                        containerColor = Color.Black,
+                        containerColor = Color(0xFFB063ED).copy(alpha = 0.76f),
                         contentColor = Color.White
                     ),
-                    shape = androidx.compose.foundation.shape.RoundedCornerShape(12.dp)
+                    shape = RoundedCornerShape(100.dp)
                 ) {
-                    Text("Login", color = Color.White)
+                    Text("Login")
                 }
             }
-
-            Spacer(modifier = Modifier.weight(1f))
 
             Text(
                 text = "Don't have an account? Register Now",
@@ -177,6 +215,8 @@ fun LoginScreen(navController: NavController) {
                     .clickable {
                         navController.navigate("registerScreen")
                     }
+                    .align(Alignment.CenterHorizontally)
+                    .padding(bottom = 16.dp)
             )
         }
     }
--- app/src/main/java/com/example/teamproject_cv2/profileScreen/ProfileScreen.kt
+++ app/src/main/java/com/example/teamproject_cv2/profileScreen/ProfileScreen.kt
@@ -7,33 +7,45 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.blur
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import com.example.teamproject_cv2.R
+import coil.compose.rememberImagePainter
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
-import java.util.*
+import java.util.UUID
 
 @Composable
 fun ProfileScreen(navController: NavController) {
@@ -41,14 +53,12 @@ fun ProfileScreen(navController: NavController) {
     val storage = FirebaseStorage.getInstance().reference
     val context = LocalContext.current
 
-    // State variables for user inputs and image URI
-    var name by remember { mutableStateOf("") }
-    var gender by remember { mutableStateOf("") }
-    var age by remember { mutableStateOf("") }
+    // 사용자 입력과 이미지 URI를 위한 상태 변수
+    var username by remember { mutableStateOf("") }
     var selectedImageUri by remember { mutableStateOf<Uri?>(null) }
     var isUploading by remember { mutableStateOf(false) }
 
-    // Image picker launcher
+    // 이미지 선택기를 위한 런처
     val imagePickerLauncher = rememberLauncherForActivityResult(
         contract = ActivityResultContracts.GetContent(),
         onResult = { uri ->
@@ -59,18 +69,48 @@ fun ProfileScreen(navController: NavController) {
     Box(
         modifier = Modifier
             .fillMaxSize()
+            .background(Color(0xFF1B0A40))
             .padding(16.dp),
         contentAlignment = Alignment.Center
     ) {
+        // 배경
+        Box(
+            modifier = Modifier
+                .size(643.28.dp, 973.34.dp)
+                .offset((-85).dp, (-69).dp)
+                .background(Color(0xFF1B0A40))
+        )
+
+        // Ellipse 5
+        Box(
+            modifier = Modifier
+                .size(407.dp, 390.dp)
+                .offset((-85).dp, (-69).dp)
+                .background(Color(0x30FF43A8), shape = CircleShape)
+                .blur(130.dp)
+                .alpha(0.8f)
+        )
+
+        // Ellipse 6
+        Box(
+            modifier = Modifier
+                .size(388.47.dp, 528.dp)
+                .offset((-571.5).dp, (-231.53).dp)
+                .background(Color(0x30D943FF), shape = CircleShape)
+                .blur(130.dp)
+                .rotate(89.96f)
+                .alpha(0.8f)
+        )
+
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center,
             modifier = Modifier.fillMaxWidth()
         ) {
-            // Display selected image or offer to pick one
+            // 선택된 이미지를 표시하거나 이미지를 선택할 수 있는 옵션을 제공
             if (selectedImageUri != null) {
                 Image(
-                    painter = painterResource(id = R.drawable.img), // Placeholder image resource
+                    painter = rememberImagePainter(selectedImageUri),
                     contentDescription = null,
                     modifier = Modifier
                         .size(128.dp)
@@ -93,83 +133,81 @@ fun ProfileScreen(navController: NavController) {
 
             Spacer(modifier = Modifier.height(16.dp))
 
-            // Text fields for user inputs
+            // 사용자 입력을 위한 텍스트 필드
             TextField(
-                value = name,
-                onValueChange = { name = it },
-                label = { Text("이름") },
-                modifier = Modifier.fillMaxWidth().padding(bottom = 16.dp)
+                value = username,
+                onValueChange = { username = it },
+                label = { Text("username") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 16.dp)
             )
-            TextField(
-                value = gender,
-                onValueChange = { gender = it },
-                label = { Text("성별") },
-                modifier = Modifier.fillMaxWidth().padding(bottom = 16.dp)
-            )
-            TextField(
-                value = age,
-                onValueChange = { age = it },
-                label = { Text("나이") },
-                modifier = Modifier.fillMaxWidth().padding(bottom = 16.dp),
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
-            )
+        }
 
-            // Button to save profile
-            Button(
-                onClick = {
-                    isUploading = true
-                    selectedImageUri?.let { uri ->
-                        // Generate a unique file name for the image
-                        val imageName = "${UUID.randomUUID()}.jpg"
-                        val imageRef = storage.child("profiles/$imageName")
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            contentAlignment = Alignment.BottomEnd
+        ) {
+            Box(
+                modifier = Modifier
+                    .width(171.dp)
+                    .height(60.dp)
+                    .clip(shape = CircleShape.copy(all = CornerSize(100)))
+                    .background(Color(0xFFB063ED))
+                    .clickable {
+                        isUploading = true
+                        selectedImageUri?.let { uri ->
+                            // 이미지 파일 이름 생성
+                            val imageName = "${UUID.randomUUID()}.jpg"
+                            val imageRef = storage.child("profiles/$imageName")
 
-                        // Upload image to Firebase Storage
-                        val uploadTask = imageRef.putFile(uri)
-                        uploadTask.continueWithTask { task ->
-                            if (!task.isSuccessful) {
-                                task.exception?.let { throw it }
+                            // 이미지 Firebase Storage에 업로드
+                            val uploadTask = imageRef.putFile(uri)
+                            uploadTask.continueWithTask { task ->
+                                if (!task.isSuccessful) {
+                                    task.exception?.let { throw it }
+                                }
+                                imageRef.downloadUrl
+                            }.addOnCompleteListener { task ->
+                                if (task.isSuccessful) {
+                                    // 업로드된 이미지의 다운로드 URL 얻기
+                                    val downloadUri = task.result
+
+                                    // 사용자 객체 생성
+                                    val user = hashMapOf(
+                                        "username" to username,
+                                        "photoUrl" to downloadUri.toString()
+                                    )
+
+                                    // Firestore에 사용자 객체 저장
+                                    firestore.collection("users")
+                                        .add(user)
+                                        .addOnSuccessListener {
+                                            Toast.makeText(context, "프로필 저장 성공", Toast.LENGTH_SHORT).show()
+                                            navController.popBackStack() // 성공 후 뒤로 가기
+                                        }
+                                        .addOnFailureListener {
+                                            Toast.makeText(context, "프로필 저장 실패: ${it.message}", Toast.LENGTH_SHORT).show()
+                                            isUploading = false
+                                        }
+                                } else {
+                                    Toast.makeText(context, "사진 업로드 실패", Toast.LENGTH_SHORT).show()
+                                    isUploading = false
+                                }
                             }
-                            imageRef.downloadUrl
-                        }.addOnCompleteListener { task ->
-                            if (task.isSuccessful) {
-                                // Get download URL of uploaded image
-                                val downloadUri = task.result
-
-                                // Create a user object with profile details and photo URL
-                                val user = hashMapOf(
-                                    "name" to name,
-                                    "gender" to gender,
-                                    "age" to age,
-                                    "photoUrl" to downloadUri.toString()
-                                )
-
-                                // Store user object in Firestore
-                                firestore.collection("users")
-                                    .add(user)
-                                    .addOnSuccessListener {
-                                        Toast.makeText(context, "프로필 저장 성공", Toast.LENGTH_SHORT).show()
-                                        navController.popBackStack() // Navigate back after success
-                                    }
-                                    .addOnFailureListener {
-                                        Toast.makeText(context, "프로필 저장 실패: ${it.message}", Toast.LENGTH_SHORT).show()
-                                        isUploading = false
-                                    }
-                            } else {
-                                Toast.makeText(context, "사진 업로드 실패", Toast.LENGTH_SHORT).show()
-                                isUploading = false
-                            }
+                        } ?: run {
+                            Toast.makeText(context, "사진을 선택해주세요", Toast.LENGTH_SHORT).show()
+                            isUploading = false
                         }
-                    } ?: run {
-                        Toast.makeText(context, "사진을 선택해주세요", Toast.LENGTH_SHORT).show()
-                        isUploading = false
-                    }
-                },
-                modifier = Modifier.fillMaxWidth().padding(top = 16.dp)
+                    },
+                contentAlignment = Alignment.Center
             ) {
                 if (isUploading) {
                     CircularProgressIndicator(color = Color.White)
                 } else {
-                    Text(text = "저장")
+                    Text(text = "저장", color = Color.White)
                 }
             }
         }
